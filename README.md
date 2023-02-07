c5e3927482b3f9425754ec62de9584873a450a5f
SalmanTahir786 <salmantahirue@gmail.com>
2023-02-07 17:38:55 +0500

    Added userauth project code

---
+++ README.md
@@ -0,0 +1,33 @@
+## Introduction
+
+This README file provides information on how to run the qt-web on your local machine.
+
+## Description 
+
+This "qt-project" is a Django REST framework project with the root directory named "qt-web" for virtual env. It contains the main root directory named "userauth" and one app directory named "account"
+
+## Prerequisites
+
+* Python 3.10
+* Django 4.1.6
+* djangorestframework 3.14
+
+## Installation
+
+#### Clone the repository to your local machine.
+
+* git clone https://github.com/meissasoft/qt-web.git
+
+#### Install the required packages using pip.
+
+* pip install -r requirements.txt
+
+## Running the Project
+
+#### Navigate to the project directory.
+
+* cd qt-web
+
+#### Run the project script using the following command.
+
+* python manage.py runserver
---
+++ userauth/userauth/urls.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Userauth API",
+      default_version='v1',
+      description="This is user login signup application created by using django REST framework",
+      terms_of_service="https://www.example.com/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/user/', include('account.urls')),
+    # path(r'^swagger(?P<format>\.json|\.yaml)$',  schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(r'^swagger(?P<format>\.json|\.yaml)$',
+         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
+]
+
+# path(r'^swagger(?P<format>\.json|\.yaml)$',
+#      schema_view.without_ui(cache_timeout=0), name='schema-json'),
+# path('', schema_view.with_ui('swagger',
+#                              cache_timeout=0), name='schema-swagger-ui'),
